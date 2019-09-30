d4650c29507ac6243a30f6877f1e0af0de5d5f11
Thomas Velle <thomas.velle@gmail.com>
2019-09-30 21:21:36 +0200

    Install query string

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,10 +3,13 @@ import '../styles/index.scss';
 import { initMark } from './mark';
 import { initThomas } from './thomas';
 import { API_KEY } from './config';
+import queryString from "query-string";
+
+const parsed = queryString.parse(location.search);
 
 // Geeft je een nummer tussen 0 en 1 terug
 const number = Math.random(); 
-if (number >= 0.5) {
+if (parsed.movieId) {
     initThomas();
 } else {
     initMark();
