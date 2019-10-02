78cdbd0d271a4f3e691672a0c963105f430fd45d
Thomas Velle <thomas.velle@gmail.com>
2019-10-02 19:17:20 +0200

    Begin details axios call

--- src/scripts/wesly.js
+++ src/scripts/wesly.js
@@ -58,6 +58,12 @@ function writeMovies(data) {
     });
 }
 
+// Functie om filmdetails op te roepen
 function getDetails() {
+    //Ophalen van de data van de specifieke film waarop geklikt is
+    Axios.get('https://api.themoviedb.org/3/discover/movie?id=' + + '&api_key=' + API_KEY)
+        .then(function(response) {
+
 
 }
+
