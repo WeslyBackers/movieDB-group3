46594ed87a3bd52f88c63c7368ce13195692ebc5
Wesly Backers <wesly.backers@gmail.com>
2019-10-02 21:46:29 +0200

    no message

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,27 +5,12 @@ import { loadMovieDetail } from './thomas';
 import { API_KEY } from './config.js';
 import queryString from "query-string";
 import Axios from "axios";
-import { get } from 'https';
 
 
 const parsed = queryString.parse(location.search);
 
-// Divs vastpakken
-var movieDetails = document.querySelector('.movieDetails');
-var overviewPage = document.querySelector('.overviewPage');
-
-
 if (parsed.movie) {
     loadMovieDetail();
-    console.log(parsed.movie);
-    
-    // Overschakelen naar Details pagina
-    movieDetails.setAttribute("hidden", false);
-    overviewPage.setAttribute("hidden", true);  
 } else {
     loadMovies();
-
-     // Overschakelen naar Overview pagina
-     movieDetails.setAttribute("hidden", true);
-     overviewPage.setAttribute("hidden", false);  
 }
