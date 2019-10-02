0154c158131b39a1f548b3b548c9bafdfaa96f5f
Thomas Velle <thomas.velle@gmail.com>
2019-10-02 21:19:41 +0200

    Switch cleaner - test

--- src/scripts/thomas.js
+++ src/scripts/thomas.js
@@ -2,10 +2,4 @@ const naam = "Thomas";
 
 export function loadMovieDetail() {
     console.log('login: ' + naam);
-
-    var movieDetails = document.querySelector('.movieDetails');
-    var overviewPage = document.querySelector('.overviewPage');
-
-    movieDetails.setAttribute("hidden", false);
-    overviewPage.setAttribute("hidden", true);  
 }
