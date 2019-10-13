a14664a6a0a486482eb9aaea6f6672efe4a5f700
WeslyBackers <wesly.backers@gmail.com>
2019-10-13 15:02:54 +0200

    kleine aanpassing: center poster en d-flex

--- src/scripts/detailPage.js
+++ src/scripts/detailPage.js
@@ -40,46 +40,26 @@ function writeMovieDetail(movieData) {
     var movieDetails = `
         <div class="jumbotron" id="jumboDetail">
             <h3 class="text-white mvTitle p-2" id="mvTitle"><b>${movieData.title}</b></h3>
-<<<<<<< HEAD:src/scripts/thomas.js
         </div>  
 
         <div class="d-flex col-12 mvInfo text-white rounded p-4 mvInfo border justify-content-between" id="movieDetailsBody">
             <div class="d-flex row col-6">
-                <div class="flex">        
+                <div class="">        
                     <p id="mvScore"><b>Rating:</b><br> ${movieData.vote_average}</p>
                     <p id="mvReleaseDate"><b>Release Date:</b><br> ${movieData.release_date}</p>
                     <p id="mvSynopsis"><b>Description:</b> <br>${movieData.overview}</p>
                     <p id="mvProductionCompaniesTitle"><b>Production Companies:</b></p>
                     <ul id="mvProductionCompanies"></ul>
                 </div>
-                <div class="flex">
+                <div class="">
                     <a class="back btn btn-light" href="index.html">Back to overview</a>
                 </div>        
             </div>
-            <div class="d-flex col-5 mvPoster" style="background-image: url('https://image.tmdb.org/t/p/w342${movieData.poster_path}');"> 
+            <div class="d-flex col-6 align-items-center mvPoster" style="background-image: url('https://image.tmdb.org/t/p/w342${movieData.poster_path}');"> 
             </div>
 
         </div>
     `;
-=======
-        </div>    
-        <div class="container mvInfo text-white rounded p-2 mvInfo border" id="movieDetailsBody>
-            <div class="posterDiv">
-                <img src="https://image.tmdb.org/t/p/w300/${movieData.poster_path}">
-            </div> <!-- End of posterDiv -->
-            <div class="infoDiv">
-                <p id="mvScore"><b>Rating:</b><br> ${movieData.vote_average}</p>
-                <p id="mvReleaseDate"><b>Release Date:</b><br> ${movieData.release_date}</p>
-                <p id="mvSynopsis"><b>Description:</b> <br>${movieData.overview}</p>
-                <p id="mvProductionCompaniesTitle"><b>Production Companies:</b></p>
-                <ul id="mvProductionCompanies"></ul>
-                <div class="d-flex justify-content-end">
-                    <a class="back btn btn-light p-1" href="index.html">Back to overview</a>
-                </div><!-- End of button div -->
-            </div><!-- End of info div -->
-        </div><!--End of movieDetailsBody div-->
-    `; // end of movieDetails variable
->>>>>>> f26f843cfa4a70cb4981297c69862d0e85b74506:src/scripts/detailPage.js
 
     // Render detais of movie on page
     detailPage.innerHTML = movieDetails;
